d52c123bd0efa7b739e1116b2489d211c78bbf46
Aldrin Mathew <admin@aldrinmathew.com>
2022-03-14 13:25:32 +0530

    Added more documentation to `TurboElement`
    - The `TurboElement` class now has more documentation
    
    - The newly added documentation is copied from the framework's own
      documentation

--- lib/turbo_element.dart
+++ lib/turbo_element.dart
@@ -5,12 +5,21 @@ class TurboElement extends ComponentElement {
   /// Creates an element that uses the given widget as its configuration.
   TurboElement(TurboWidget widget) : super(widget);
 
+  /// The configuration for this element
   @override
   TurboWidget get widget => super.widget as TurboWidget;
 
+  /// Subclasses should override this function to actually call the appropriate
+  /// `build` function (e.g., [StatelessWidget.build] or [State.build]) for
+  /// their widget.
   @override
   Widget build() => widget.build(this);
 
+  /// Change the widget used to configure this element.
+  ///
+  /// The framework calls this function when the parent wishes to use a different widget to configure this element. The new widget is guaranteed to have the same [runtimeType] as the old widget.
+  ///
+  /// This function is called only during the "active" lifecycle state.
   @override
   void update(TurboWidget newWidget) {
     super.update(newWidget);
@@ -19,6 +28,24 @@ class TurboElement extends ComponentElement {
     rebuild();
   }
 
+  /// Transition from the "inactive" to the "defunct" lifecycle state.
+  ///
+  /// Called when the framework determines that an inactive element will never
+  /// be reactivated. At the end of each animation frame, the framework calls
+  /// [unmount] on any remaining inactive elements, preventing inactive elements
+  /// from remaining inactive for longer than a single animation frame.
+  ///
+  /// After this function is called, the element will not be incorporated into
+  /// the tree again.
+  ///
+  /// Any resources this element holds should be released at this point. For
+  /// example, [RenderObjectElement.unmount] calls [RenderObject.dispose] and
+  /// nulls out its reference to the render object.
+  ///
+  /// See the lifecycle documentation for [Element] for additional information.
+  ///
+  /// Implementations of this method should end with a call to the inherited
+  /// method, as in `super.unmount()`.
   @override
   @mustCallSuper
   void unmount() {
